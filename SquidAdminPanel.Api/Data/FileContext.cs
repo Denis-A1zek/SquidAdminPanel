d2beb7d9913ab343c6a9dc2c35a4bd343d461418
Denis-A1zek <denissigida.work@gmail.com>
2023-04-22 11:30:36 +0300

    fix context

--- SquidAdminPanel.Api/Data/FileContext.cs
+++ SquidAdminPanel.Api/Data/FileContext.cs
@@ -3,7 +3,18 @@
 public class FileContext
 {
     public string _path = string.Empty;
-    public FileContext(string path) => _path = path;
+    
+    /// <summary>
+    /// Creates a file context in wich you can make queries
+    /// </summary>
+    /// <param name="path">Path to file</param>
+    /// <exception cref="FileNotFoundException">If file not exsist</exception>
+    public FileContext(string path)
+    {
+        if (File.Exists(path) == false)
+            throw new FileNotFoundException($"Файл по пути {path} не был найден");
+        _path = path;
+    }  
     
     public async Task<TResult> QueryReadLineAsync<TResult>(Func<TResult> initializer,Action<TResult> action)
     {
@@ -18,7 +29,6 @@ public class FileContext
 
         return result;
     }
-
     public async Task<TResult> QueryReadAllLineAsync<TResult>(Func<string[], TResult> action)
     {
         string[] fileText = await File.ReadAllLinesAsync(_path);
