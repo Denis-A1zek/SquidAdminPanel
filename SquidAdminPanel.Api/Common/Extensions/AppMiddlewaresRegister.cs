ba2bb65df87e2a7e39cbce236c36213a33489b4c
Denis-A1zek <denissigida.work@gmail.com>
2023-04-22 11:30:20 +0300

    changed the path

---
+++ SquidAdminPanel.Api/Common/Extensions/AppMiddlewaresRegister.cs
@@ -0,0 +1,13 @@
+﻿namespace SquidAdminPanel.Api.Common.Extensions;
+
+public static class AppMiddlewaresRegister
+{
+    public static void RegisterMiddleware(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+    }
+}
---
+++ SquidAdminPanel.Api/Common/Extensions/ServiceRegisterExtension.cs
@@ -0,0 +1,15 @@
+﻿using SquidAdminPanel.Api.Api;
+using SquidAdminPanel.Api.Api.Interfaces;
+
+namespace SquidAdminPanel.Api.Common.Extensions;
+
+public static class ServiceRegisterExtension
+{
+    public static void AddServices(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddTransient<IApi, TestApi>();
+    }
+}
