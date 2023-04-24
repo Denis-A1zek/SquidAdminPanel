c1edb4b9a62ccc28a9c2c9641c2383bfd2cf4e8d
Denis-A1zek <denissigida.work@gmail.com>
2023-04-24 17:31:59 +0300

    Create user api

---
+++ SquidAdminPanel.Api/Application/Api/UserApi.cs
@@ -0,0 +1,65 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SquidAdminPanel.Api.Core;
+using SquidAdminPanel.Api.Core.Models;
+using SquidAdminPanel.Api.Core.Users;
+using SquidAdminPanel.Api.Core.Users.Commands.CreateUser;
+using SquidAdminPanel.Api.Core.Users.Commands.DeleteUser;
+
+namespace SquidAdminPanel.Api.Application;
+
+public class UserApi : IApi
+{
+    public void Register(WebApplication app)
+    {
+        app.MapGet("api/users", GetAll)
+            .Produces<List<User>>(StatusCodes.Status200OK)
+            .WithName("GetAllUsers")
+            .WithTags("User");
+
+        app.MapPatch("api/user", Post)
+            .Produces<string>(StatusCodes.Status200OK)
+            .WithName("CreateUser")
+            .WithTags("User");
+
+        app.MapDelete("api/user/{userName}", Delete)
+            .Produces<string>(StatusCodes.Status200OK)
+            .WithName("DeleteUser")
+            .WithTags("User");
+    }
+
+
+
+    /// <summary>
+    /// Gets users 
+    /// </summary>
+    /// <remarks>
+    /// GET /api/users
+    /// </remarks>
+    /// <param name="mediator"></param>
+    /// <returns>Returns the users of the proxy server</returns>
+    /// <response code="200">Success</response>
+    private async Task<IResult> GetAll([FromServices]IMediator mediator) =>
+        Results.Ok(await mediator.Send(new GetUsersQuery()));
+
+    /// <summary>
+    /// Creates a new user
+    /// </summary>
+    /// <remarks>
+    /// POST api/user/
+    /// </remarks>
+    /// <param name="user">User</param>
+    /// <param name="mediator"></param>
+    /// <returns>Username</returns>
+    private async Task<IResult> Post(UserRequest user, [FromServices] IMediator mediator) =>
+        Results.Ok(await (mediator.Send(new CreateUserCommand(user.Name, user.Password))));
+
+    /// <summary>
+    /// Deletes a user from the proxy
+    /// </summary>
+    /// <param name="userName">Username</param>
+    /// <param name="mediator"></param>
+    /// <returns>Username</returns>
+    private async Task<IResult> Delete(string userName, [FromServices] IMediator mediator) =>
+        Results.Ok(await mediator.Send(new DeleteUserCommand(userName)));
+}
--- SquidAdminPanel.Api/Application/Common/Extensions/ServiceRegisterExtension.cs
+++ SquidAdminPanel.Api/Application/Common/Extensions/ServiceRegisterExtension.cs
@@ -9,20 +9,28 @@ namespace SquidAdminPanel.Api.Application;
 
 public static class ServiceRegisterExtension
 {
-    public static void AddServices(this IServiceCollection services)
+    public static void AddServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
 
+        //TO-DO Get all classes from assembley with interface IApi and inject into services
         services.AddTransient<IApi, TestApi>();
+        services.AddTransient<IApi, UserApi>();
+
         Assembly.GetAssembly(typeof(ProcessManager))
                 .GetTypes().Where(type => type.IsSubclassOf(typeof(ProcessManager)))
                 .ToList().ForEach(type =>
                 {
                     services.AddScoped(type);
-                }); ;
+                });
 
         services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        services.AddScoped<UserContext>(factory =>
+        {
+            return new UserContext(configuration[nameof(UserContext)]);
+        });
     }
 }
 
