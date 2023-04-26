279574426e811f63400fcf88657a2120642444e2
Denis-A1zek <denissigida.work@gmail.com>
2023-04-26 13:39:11 +0300

    create console view

--- SquidAdminPanel.WebUI/js/site.js
+++ SquidAdminPanel.WebUI/js/site.js
@@ -2,3 +2,44 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(function(){
+      var id = uuidv4();
+      var source = new EventSource("https://localhost:7075/streamlogs/" + id);
+
+      source.onmessage = function(event){            
+            const logs = JSON.parse(event.data);
+            const line = logs.Logs;
+            if(line.length != 0)
+            {
+                  for(log in line){
+                        //document.getElementById("spnMessage").innerHTML +=
+                        const logmessage = line[log].Time + line[log].User + line[log].Address + line[log].StatusCode + 
+                              line[log].Description + line[log].FromAddress + "\n";
+
+                        //const someValue = uuidv4();
+                        $('#tab').append('<p>'+ logmessage +'</p>');
+                        
+                        //$('#'+someValue).css('color','red');
+
+                        //setTimeout(function() {
+                         //     $('#'+someValue).css('color', 'black');
+
+                          //  }, 500);
+                        
+
+                  }
+                  $('#tab').scrollTop($('#tab')[0].scrollHeight);
+                  console.log("Получение данных с сервера");
+            }
+
+      }     
+
+      
+      function uuidv4() {
+            return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+            );
+      }
+});
