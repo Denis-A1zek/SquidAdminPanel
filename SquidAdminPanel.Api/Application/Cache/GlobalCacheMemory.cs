e1370a8a6965c0757e5a551eedca8d5e33414bcf
Denis-A1zek <denissigida.work@gmail.com>
2023-04-26 11:21:03 +0300

    add global cache

---
+++ SquidAdminPanel.Api/Application/Cache/GlobalCacheMemory.cs
@@ -0,0 +1,40 @@
+﻿namespace SquidAdminPanel.Api.Application.Cache
+{
+    public class GlobalCacheMemory : IGlobalCacheMemory
+    {
+        private Dictionary<Guid, string> _lastLogTimeValue = new();
+
+        public bool IsExsist(Guid id) => _lastLogTimeValue.ContainsKey(id);
+
+        public string GetLastValue(Guid id)
+        {
+            CreateIfNotExsist(id);
+
+            return _lastLogTimeValue[id];
+        }
+
+        public void SetValue(Guid id, string value)
+        {
+            CreateIfNotExsist(id);
+
+            _lastLogTimeValue[id] = value;
+        }
+
+        private void CreateIfNotExsist(Guid id)
+        {
+            if (!IsExsist(id))
+            {
+                CreateCache(id);
+            }
+        }
+
+        private bool CreateCache(Guid id)
+        {
+            if (_lastLogTimeValue.ContainsKey(id))
+                return false;
+
+            _lastLogTimeValue.Add(id, "0");
+            return true;
+        }
+    }
+}
---
+++ SquidAdminPanel.Api/Application/Cache/IGlobalCacheMemory.cs
@@ -0,0 +1,9 @@
+﻿namespace SquidAdminPanel.Api.Application.Cache
+{
+    public interface IGlobalCacheMemory
+    {
+        public bool IsExsist(Guid id);
+        public string GetLastValue(Guid id);
+        public void SetValue(Guid id, string value);
+    }
+}
