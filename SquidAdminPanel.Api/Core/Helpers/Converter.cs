6102631e0834498024f279c475521bbef80f4c0c
Denis-A1zek <denissigida.work@gmail.com>
2023-04-26 08:27:03 +0300

    add converter

---
+++ SquidAdminPanel.Api/Core/Helpers/Converter.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace SquidAdminPanel.Api.Core
+{
+    public static class Converter
+    {
+        public static DateTime SecondsToDateConverter(string value)
+        {
+            double seconds = Convert.ToDouble(value, new CultureInfo(CultureInfo.InvariantCulture.Name));
+            var time = TimeSpan.FromSeconds(seconds);
+            return new DateTime(1970, 01, 01).Add(time); ;
+        }
+    }
+}
